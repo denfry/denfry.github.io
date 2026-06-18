747aaf594f5da9822b5398f9c611c3b6668a32dc
denfry <aseraw115@gmail.com>
2026-06-18 16:30:24 +0300

    feat: compose full page in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { Header } from './components/Header/Header'
+import { Intro } from './components/Intro/Intro'
+import { Work } from './components/Work/Work'
+import { LabeledSection } from './components/LabeledSection/LabeledSection'
+import { Footer } from './components/Footer/Footer'
+import { usePrefs } from './context/PrefsContext'
+import { STRINGS } from './i18n'
+
 export default function App() {
-  return <div className="shell">scaffold ok</div>
+  const { lang } = usePrefs()
+  const t = STRINGS[lang]
+  return (
+    <div className="shell">
+      <Header />
+      <Intro />
+      <Work />
+      <LabeledSection label={t.focusLabel} text={t.focusText} />
+      <LabeledSection label={t.backgroundLabel} text={t.backgroundText} />
+      <Footer />
+    </div>
+  )
 }
