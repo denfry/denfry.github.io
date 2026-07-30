647d3b2918d0c040effc81128d090628bd3da673
denfry <aseraw115@gmail.com>
2026-07-30 10:37:05 +0300

    feat: mount wireframe scene behind translucent content panels

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Intro } from './components/Intro/Intro'
 import { Work } from './components/Work/Work'
 import { LabeledSection } from './components/LabeledSection/LabeledSection'
 import { Footer } from './components/Footer/Footer'
+import { Scene } from './components/Scene/Scene'
 import { usePrefs } from './context/PrefsContext'
 import { STRINGS } from './i18n'
 
@@ -10,13 +11,16 @@ export default function App() {
   const { lang } = usePrefs()
   const t = STRINGS[lang]
   return (
-    <div className="shell">
-      <Header />
-      <Intro />
-      <Work />
-      <LabeledSection label={t.focusLabel} text={t.focusText} />
-      <LabeledSection label={t.backgroundLabel} text={t.backgroundText} />
-      <Footer />
-    </div>
+    <>
+      <Scene />
+      <div className="shell">
+        <Header />
+        <Intro />
+        <Work />
+        <LabeledSection label={t.focusLabel} text={t.focusText} />
+        <LabeledSection label={t.backgroundLabel} text={t.backgroundText} />
+        <Footer />
+      </div>
+    </>
   )
 }
